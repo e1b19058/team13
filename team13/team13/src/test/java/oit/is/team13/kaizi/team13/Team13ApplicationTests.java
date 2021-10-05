b4170f8344706ac86daef024be0c363562ab6e6f
e1b19058 <e1b19058@oit.ac.jp>
2021-10-05 14:40:37 +0900

    リポジトリの初期化

---
+++ team13/team13/src/test/java/oit/is/team13/kaizi/team13/Team13ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team13.kaizi.team13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
